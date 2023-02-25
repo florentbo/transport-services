4e5481f346de93c5333f27e131d7d8d631092473
fbonamis <florent.bonamis@ppretail.com>
2023-02-25 10:26:44 +0100

    swagger open api setup OK

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import be.bonamis.transport.api.PlacesApi;
+import be.bonamis.transport.model.Place;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GreetingController implements PlacesApi {
+
+    @Override
+    public ResponseEntity<List<Place>> placesGet() {
+        Place place = new Place();
+        place.setId(99L);
+        return ResponseEntity.ok(List.of(place));
+    }
+
+    @Override
+    public ResponseEntity<Place> placesPlaceIdGet(String placeId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> placesPost() {
+        return null;
+    }
+}
