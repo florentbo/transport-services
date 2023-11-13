83cc6383cfc37ce96e582b1c4ee6ffd67fdf90df
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2023-11-13 13:07:26 +0100

    using the operationId to have better controller method names

--- src/main/java/com/example/demo/PlacesController.java
+++ src/main/java/com/example/demo/PlacesController.java
@@ -2,31 +2,19 @@ package com.example.demo;
 
 import be.bonamis.transport.api.PlacesApi;
 import be.bonamis.transport.model.Place;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @RestController
 public class PlacesController implements PlacesApi {
-
-    @Override
-    public ResponseEntity<List<Place>> placesGet() {
-        Place place = new Place();
-        place.setId(99L);
-        place.setName("dummy name");
-        place.setPostedDate(LocalDate.of(2023,11,15));
-        return ResponseEntity.ok(List.of(place));
-    }
-
-    @Override
-    public ResponseEntity<Place> placesPlaceIdGet(String placeId) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Void> placesPost() {
-        return null;
-    }
+  @Override
+  public ResponseEntity<List<Place>> findPlaces() {
+    Place place = new Place();
+    place.setId(99L);
+    place.setName("dummy name");
+    place.setPostedDate(LocalDate.of(2023, 11, 15));
+    return ResponseEntity.ok(List.of(place));
+  }
 }
